b8c8c1f15e320cfe2caf54f42a934dfefc5e71fd
Jordi Galobart Salvat <jgalobart@gmail.com>
2022-03-02 21:06:55 +0100

    add port view

--- app.js
+++ app.js
@@ -6,13 +6,17 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/port', (req, res) => {
+    res.send(process.env.PORT)
+  })
+
 app.post('/', (req, res) => {
     res.send('Bye World!')
   })
 
-  app.post('/lists/list', (req, res) => {
-    res.send('List!')
-  })
+app.post('/lists/list', (req, res) => {
+res.send('List!')
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
